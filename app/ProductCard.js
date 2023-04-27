27459a31fbe736d044c903ed0a9edf1f5f4f355a
JRH89 <thegemlins@gmail.com>
2023-04-27 06:14:20 -0700

    ok

--- app/ProductCard.js
+++ app/ProductCard.js
@@ -2,6 +2,7 @@
 import React from "react"
 import { useRouter } from "next/navigation"
 import useCart from "./(store)/store"
+import Image from "next/image"
 
 export default function ProductCard(props) {
     const { product } = props
@@ -27,7 +28,7 @@ export default function ProductCard(props) {
 
     return (
     <div onClick={onProductClick} className="flex flex-col shadow bg-white hover:shadow-lg cursor-pointer">
-        <img src={productInfo.images[0]} alt={name} className="w-full h-full object-cover" />
+        <Image src={productInfo.images[0]} alt={name} className="w-full h-full object-cover" />
          
         <div className="flex flex-col gap-2 p-4">
             <div className="flex items-center justify-center justify-between">
