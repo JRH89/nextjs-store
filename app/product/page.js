504e4d1ccfb454e00c412fc4f767e30d5a6945da
JRH89 <thegemlins@gmail.com>
2023-05-08 00:37:10 -0700

    ok

--- app/product/page.js
+++ app/product/page.js
@@ -25,7 +25,6 @@ export default function ProductPage(props) {
         addItemToCart({ newItem })
     }
 
-
     return (
         <div className="flex flex-col p-4">
             <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[1000px] mx-auto">
