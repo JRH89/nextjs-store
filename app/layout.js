6bcc183c15e5b4813e9e274177732248a821a271
JRH89 <thegemlins@gmail.com>
2023-06-03 00:00:37 -0700

    ok

--- app/layout.js
+++ app/layout.js
@@ -17,30 +17,30 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta property="og:url" content="https://next-jewelry.vercel.app/" />
-        <meta property="og:title" content="Jewelry Store" />
-        <meta property="og:description" content="E-commerce app made with NextJS 13, TailwindCSS, Stripe, and Zustand" />
+        <meta property="og:title" content="Jewelry & Gems" />
+        <meta property="og:description" content="Silver jewelry and gemstone e-commerce website" />
         <meta property="og:image" content="https://raw.githubusercontent.com/JRH89/nextjs-store/main/public/jewelry-cover.png" />
         <meta property="url" content="https://next-jewelry.vercel.app/" />
-        <meta property="title" content="Jewelry Store" />
-        <meta property="description" content="E-commerce app made with NextJS 13, TailwindCSS, Stripe, and Zustand" />
+        <meta property="title" content="Jewelry & Gems" />
+        <meta property="description" content="Silver jewelry and gemstone e-commerce website" />
         <meta property="image" content="https://raw.githubusercontent.com/JRH89/nextjs-store/main/public/jewelry-cover.png" />
         <link type="image/x-icon" rel="shortcut icon" href="favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className={'min-h-screen flex flex-col relative ' + inter.className}>
-       <Header />
+        <Header />
         <div className='flex-1'>
           {children}
         </div>
         <footer className='flex items-center flex-wrap justify-center border-t border-solid border-slate-900 p-4 md:p-8 gap-4'>
           <Link href={'https://next-mycard.vercel.app/Dynamic?name=Jared%20Hooker&job=Javascript%20Developer&phone=19096473928&email=gamedevjared%40gmail.com&social1=https%3A%2F%2Flinkedin.com%2Fin%2Fjaredhooker&social2=https%3A%2F%2Fgithub.com%2FJRH89&social3=https%3A%2F%2Fjaredsportfolio.netlify.app&social4=https%3A%2F%2Fplay.google.com%2Fstore%2Fsearch%3Fq%3Dhooker%2520hill%2520studios%26c%3Dapps&social1Label=LinkedIN&social2Label=Github&social3Label=Portfolio&social4Label=Google%20Play'} target='_blank'>
-          <i className="fa-solid fa-envelope text-slate-900 hover:text-slate-500 cursor-pointer text-2xl sm:text-3xl md:text-4xl"></i>
+            <i className="fa-solid fa-envelope text-slate-900 hover:text-slate-500 cursor-pointer text-2xl sm:text-3xl md:text-4xl"></i>
           </Link>
           <Link href={'https://jaredsportfolio.netlify.app'} target='_blank'>
-          <i className="fa-solid fa-house text-slate-900 hover:text-slate-500 cursor-pointer text-2xl sm:text-3xl md:text-4xl"></i>
+            <i className="fa-solid fa-house text-slate-900 hover:text-slate-500 cursor-pointer text-2xl sm:text-3xl md:text-4xl"></i>
           </Link>
           <Link href={'https://linkedin.com/in/jaredhooker'} target='_blank'>
-          <i className="fa-brands fa-linkedin text-slate-900 hover:text-slate-500 cursor-pointer text-2xl sm:text-3xl md:text-4xl"></i>
+            <i className="fa-brands fa-linkedin text-slate-900 hover:text-slate-500 cursor-pointer text-2xl sm:text-3xl md:text-4xl"></i>
           </Link>
         </footer>
         <div id='portal'></div>
