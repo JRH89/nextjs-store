4d9cd5eff0d0639f9afa9777cc0cd3e27efa5771
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-06-20 18:34:35 -0700

    Update page.js

--- app/success/page.js
+++ app/success/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function SuccessPage() {
     return (
         <div className="flex flex-col text-center pt-10 justify-center items-center">
-            Thank you for your purchase!<br />
+            Thank you for your purchase! -Jared Hooker<br />
             <Link className="flex-1 mt-5 w-1/3 p-2 text-white hover:text-slate-700 bg-black rounded shadow-xl border border-solid hover:opacity-60 hover:scale-110" href="/">Back Home</Link>
         </div>
     )
