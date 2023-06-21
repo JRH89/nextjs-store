f9860e62de5952ef5883c1b63d0e4810b8af1faa
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-06-20 18:29:43 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,11 +19,11 @@ export default function RootLayout({ children }) {
         <meta property="og:url" content="https://next-jewelry.vercel.app/" />
         <meta property="og:title" content="Jewelry & Gems" />
         <meta property="og:description" content="Silver jewelry and gemstone e-commerce website" />
-        <meta property="og:image" content="https://raw.githubusercontent.com/JRH89/nextjs-store/main/public/jewelry-cover.png" />
+        <meta property="og:image" content="https://next-jewelry.vercel.app/jewelry-cover.png" />
         <meta property="url" content="https://next-jewelry.vercel.app/" />
         <meta property="title" content="Jewelry & Gems" />
         <meta property="description" content="Silver jewelry and gemstone e-commerce website" />
-        <meta property="image" content="https://raw.githubusercontent.com/JRH89/nextjs-store/main/public/jewelry-cover.png" />
+        <meta property="image" content="https://next-jewelry.vercel.app/jewelry-cover.png" />
         <link type="image/x-icon" rel="shortcut icon" href="favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
